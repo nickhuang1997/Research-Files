2ea7b971cee4394f84b9792cd7e3d3da70a43be5
NickHuangLaptop <njgh002@bucknell.edu>
2017-05-22 14:13:14 -0400

    Final sailor Code

--- Practice Codes/Drunken Sailors (Many).py
+++ Practice Codes/Drunken Sailors (Many).py
@@ -7,13 +7,17 @@ Created on Wed May 17 22:17:07 2017
 
 #Walk Funct
 import random
+import numpy as np
+np.set_printoptions(threshold=np.inf)           #shows entire array. does not have ... in it
+import matplotlib.pyplot as plt
 
 
-def sailorwalk():
-    nsteps = int(input('How many steps is the sailor going to take? '))
-    pos = 0                                     # initial position of sailor    
-    for z in range(0, nsteps+1):
+
+def walkfunc(nsteps):
     
+    pos = 0                                     # initial position of sailor    
+    
+    for z in range(0, nsteps):
         FwdOrBack = random.choice([1,2])        #1 = fwd 2 = back
         
         if FwdOrBack == 1:
@@ -21,18 +25,21 @@ def sailorwalk():
        
         else:
             pos = pos - 1
-        #print(pos)
-    return 'The final position is '+str(pos)
+        
+    return pos
 
 
-#matlab version       
-#Endpos = 0;
-#    for k = 1:amountofsteps
-#        Startpos = 0 + Endpos; %starting position of 0
-#
-#        Move = [-1 1]   ;
-#        Step = Move(randi(numel(Move)));%move +1 or -1
-#
-#        Endpos = Startpos + Step;  % Adding +-1 to current post
-#    end
-#end
+def manysailors():
+    sailors = int(input('How many sailors are there? '))
+    nsteps = int(input('How many steps will each sailor take? '))
+    arraypos = np.zeros(sailors)                   #creates an array with the same amount of slots as sailors
+    for i in range(0, sailors):
+        endpos = walkfunc(nsteps)
+        arraypos[i] = endpos
+    
+#    bins = np.arange(-nsteps,nsteps)
+    plt.xlabel('Final Position')
+    plt.ylabel('Number of sailors')
+    plt.hist(arraypos,bins ='auto',histtype="bar")          #could use bins = 'auto'
+    plt.savefig('users/nick/desktop/Many_sailors.png')
+       

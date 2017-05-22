705c9381d90f4dbcc8ff05cd91000a57413a3e2b
nickhuang1997 <njgh002@bucknell.edu>
2017-05-22 13:46:59 -0400

    Add files via upload

---
+++ Main Model.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 19 16:47:24 2017
+
+@author: njgh002
+"""
+
+#==============================================================================
+#Sedimentation Model 
+#==============================================================================
+
+#variables
+rk  = 1      #for rising particle of radius k 
+vk  = 
+ak  =        #area of rising particle. which is spherical
+c1  =        #concentration of solution
+
+#this is a test to see if git works.
+
+#testing to push this change
+#blah blah blah
+
+#This is a test from laptop spyder

c6d5f4e68acb55c1921ad845117b808f18243ae2
Nick J. Huang <njgh002@bucknell.edu>
2017-05-23 17:07:42 -0400

    Finance Model-Options

---
+++ Practice Codes/Black-Scholes.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 23 11:35:33 2017
+
+@author: njgh002
+"""
+
+#Black Scholes Model
+
+def 
+
+def optionpremium():
+    
+    S = int(input('What is the currentprice of the stock? '))       #stock price
+    K = int(input('What is the strike price of the option? '))
+    r = int(input('What is the Risk-Free interest rate?' ))
+    t = int(input('What is the time until option exercise? (years) '))
+    

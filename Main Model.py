b8cdf6b5931249ff9017169d00b469272af0fa6e
NickHuangLaptop <njgh002@bucknell.edu>
2017-05-23 11:34:10 -0400

    Added Crappy Sedimentation

--- Main Model.py
+++ Main Model.py
@@ -4,7 +4,9 @@ Created on Fri May 19 16:47:24 2017
 
 @author: njgh002
 """
-
+import numpy as np
+np.set_printoptions(threshold=np.inf)           #shows entire array. does not have ... in it
+import matplotlib.pyplot as plt
 #==============================================================================
 #Sedimentation Model 
 # Assumptions
@@ -12,22 +14,50 @@ Created on Fri May 19 16:47:24 2017
 #   2. bubbles float straight verticle
 #==============================================================================
 
-#Radius         model
-def rk():             #for rising particle of radius k 
 
 
+vel_k       = .01
 
-#Velocity       model 
-def vel_k():
+def sedimentation():
+    arrayradius = np.zeros(5)                   #creates an array with the same amount of slots as sailors
+    radius_k    = .001
+    timestep    = .0001
+    endtime     = 2
+    
+    for i in np.arange(0,endtime,timestep):
+        radius_k += .04
+        arrayradius[i] = radius_k
+    
+    plt.xlabel('Radius of Particle')
+    plt.ylabel('Number of Occurances')
+    plt.hist(arrayradius,bins =5,histtype="bar")          #could use bins = 'auto'
 
+sedimentation()
+#    arraypos = np.zeros(sailors)                   #creates an array with the same amount of slots as sailors
+#    for i in range(0, sailors):
+#        endpos = walkfunc(nsteps)
+#        arraypos[i] = endpos
+#        
+#        vel_k    += 
+        
+        
 
-
-#Area           model
-def ak(rk):                 #area of rising particle. which is spherical
-
-
-
-#concentration  model
-c1  =        #concentration of solution
-
-
+##Radius         model
+#def rk():             #for rising particle of radius k 
+#
+#
+#
+##Velocity       model 
+#def vel_k():
+#
+#
+#
+##Area           model
+#def ak(rk):                 #area of rising particle. which is spherical
+#
+#
+#
+##concentration  model
+#c1  =        #concentration of solution
+#
+#

534d8f5ca5d690dd16516520131b4d2d06e32945
Nick J. Huang <njgh002@bucknell.edu>
2017-05-25 12:14:13 -0400

    Created Crappy Sedimentation Model
    uses ODEs and yeets

--- Main Model.py
+++ Main Model.py
@@ -31,33 +31,3 @@ def sedimentation():
     plt.xlabel('Radius of Particle')
     plt.ylabel('Number of Occurances')
     plt.hist(arrayradius,bins =5,histtype="bar")          #could use bins = 'auto'
-
-sedimentation()
-#    arraypos = np.zeros(sailors)                   #creates an array with the same amount of slots as sailors
-#    for i in range(0, sailors):
-#        endpos = walkfunc(nsteps)
-#        arraypos[i] = endpos
-#        
-#        vel_k    += 
-        
-        
-
-##Radius         model
-#def rk():             #for rising particle of radius k 
-#
-#
-#
-##Velocity       model 
-#def vel_k():
-#
-#
-#
-##Area           model
-#def ak(rk):                 #area of rising particle. which is spherical
-#
-#
-#
-##concentration  model
-#c1  =        #concentration of solution
-#
-#
--- Practice Codes/Black-Scholes.py
+++ Practice Codes/Black-Scholes.py
@@ -7,12 +7,64 @@ Created on Tue May 23 11:35:33 2017
 
 #Black Scholes Model
 
-def 
+#def 
+#
+#def optionpremium():
+#    
+#    S = int(input('What is the currentprice of the stock? '))       #stock price
+#    K = int(input('What is the strike price of the option? '))
+#    r = int(input('What is the Risk-Free interest rate?' ))
+#    t = int(input('What is the time until option exercise? (years) '))
 
-def optionpremium():
-    
-    S = int(input('What is the currentprice of the stock? '))       #stock price
-    K = int(input('What is the strike price of the option? '))
-    r = int(input('What is the Risk-Free interest rate?' ))
-    t = int(input('What is the time until option exercise? (years) '))
-    
+""" # The Black Scholes Formula
+# CallPutFlag - This is set to 'c' for call option, anything else for put
+# S - Stock price
+# K - Strike price
+# T - Time to maturity
+# r - Riskfree interest rate
+# d - Dividend yield
+# v - Volatility
+"""
+from scipy.stats import norm
+from math import *
+def BlackScholes(CallPutFlag,S,K,T,r,d,v):
+    d1 = (log(float(S)/K)+((r-d)+v*v/2.)*T)/(v*sqrt(T))
+    d2 = d1-v*sqrt(T)
+    if CallPutFlag=='c':
+        return S*exp(-d*T)*norm.cdf(d1)-K*exp(-r*T)*norm.cdf(d2)
+    else:
+        return K*exp(-r*T)*norm.cdf(-d2)-S*exp(-d*T)*norm.cdf(-d1)
+
+
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.clf()
+fig,ax = plt.subplots()
+maturity = 0
+S = np.linspace(80,120,200)
+p = []
+for i in S:
+    p.append(BlackScholes('p', i, 100, 0.005, 0.06, 0, 0.4))
+
+line, = ax.plot(p)
+#ax.set_ylim()
+
+
+def update(step):
+    p = []
+    for i in S:
+        p.append(BlackScholes('p', i, 100, step, 0.06, 0, 0.4))
+        line.set_ydata(p)
+def data_gen():
+    expStop = 0.0005
+    expStart = 1.5
+    T = np.linspace(expStop,expStart,200)
+    m = -log(expStop/expStart)/expStart
+    for t in T:
+        yield expStart*exp(-m*t)
+
+ani = animation.FuncAnimation(fig, update, data_gen, interval=100)
+
+plt.show()    

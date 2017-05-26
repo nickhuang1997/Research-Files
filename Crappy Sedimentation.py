8da9206bcd5919fb2f67370ddd96834664e46a07
Nick J. Huang <njgh002@bucknell.edu>
2017-05-26 09:03:07 -0400

    commit

--- Crappy Sedimentation.py
+++ Crappy Sedimentation.py
@@ -8,16 +8,16 @@ Created on Thu May 25 11:37:40 2017
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-
+np.set_printoptions(threshold=np.inf) #shows entire array. does not have ... in it
 
 
 def f(m):
     sigma   = 1000      #M/min
-    endtime =
-    timestep=
+    endtime = 2
+    timestep= 100
     array   = np.zeros(timestep)
     
-    for i in np.arange(0 , endtime, timestep)
+    for i in np.arange(0 , endtime, timestep):
         
         dmdt     = sigma - r(m)
         array[i] = dmdt

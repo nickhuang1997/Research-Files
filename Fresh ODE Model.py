5c72a002f37beb55f5149944e6c06e927f60c525
NickHuangLaptop <njgh002@bucknell.edu>
2017-06-12 09:39:49 -0400

    commit to master

--- Fresh ODE Model.py
+++ Fresh ODE Model.py
@@ -13,10 +13,46 @@ import matplotlib.pyplot as plt
 def smodel():
     """Runs entire model"""
     
-    m = 10**-6
-
-
-def arraymaker(numarray,endtime,timestep):
+    sigma   = 1      #M/min
+    m       =10**-6    # num of monomers
+    endtime = 5
+    timestep= .1
+    
+    numarray = int(input('How many arrays of data are you comparing? '))
+    xarrays = arraymaker(numarray,endtime,timestep)
+    
+    for i in range(0,len(xarrays[0])):
+        xarrays[i][i]  = linearODE(sigma,m)
+        m              = m + dmdt1
+        
+    x = range(0,len(xarrays[0]))
+    y = xarrays[0]
+    
+    y2= xarrays[1]
+    print(y)
+    print(y2)
+    
+    
+    plt.plot(x,y, label = 'Gamma Line')
+    plt.plot(x,y2, label = "m^2 Line")
+    plt.xlabel('Time (min)')
+    plt.ylabel('Concentration... in mM')
+    plt.legend()
+    plt.title('Rate of change of concentration of monomer LTB4 over time')    
+#        for z in range(0,numarray):
+            
+#            xarrays[i][z]  = nonlinearODE(sigma,m) 
+               
+#    plt.plot(x,y, label = 'Gamma Line')
+#    plt.plot(x,y2, label = "m^2 Line")
+#    plt.xlabel('Time (min)')
+#    plt.ylabel('Concentration... in mM')
+#    plt.legend()
+#    plt.title('Rate of change of concentration of monomer LTB4 over time')
+        
+        
+    
+def arraymaker(numarray,endtime,timestep): 
     """creates n number of arrays based on 
     the endtime and timestep that is specified"""    
     
@@ -26,4 +62,22 @@ def arraymaker(numarray,endtime,timestep):
         manyarrays[i] = array
     return manyarrays
 
-arraymaker(3,4,2)
+
+
+def linearODE(sigma, m):
+    """calculates the m * g"""
+    g     = 0.278
+    fmd1  = g * m
+    dmdt1 = sigma - fmd1
+    return dmdt1
+
+
+    
+def nonlinearODE(sigma, m):
+    """calculates value for nonlinear ode"""
+    c1   = 1
+    fmd2 = 2 * c1 * m**2
+    dmdt2= sigma - fmd2
+    return dmdt2
+
+    

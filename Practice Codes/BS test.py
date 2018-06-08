0d5baaf08a11ab344727c8eeb947a2dee81f31ba
NickHuangLaptop <njgh002@bucknell.edu>
2018-06-08 11:49:46 -0400

    commit to branch
    added in all user inputs. ITS DONE BABY

--- Practice Codes/BS test.py
+++ Practice Codes/BS test.py
@@ -12,35 +12,49 @@ import matplotlib.animation as animation
 
 from math import *
 from scipy.stats import norm
-from matplotlib.ticker import Formatter #for axis formatting obviously
 
 def main():
     """
-    this function sets up the matrix and has 
-    increment values for the Y and X axis
+    uses all helper functions to run program
+    takes the following inputs...
+    1) Spot price range and step value
+    2) Volatility range
+    3) Strike price 
+    4) Risk free rate
+    5) Dividend Yield
+    6) Days left until expiry
+    7) 
     """
-    ans             = '196 197' #input('Lower and upper stock prices you are examining? ')
-    ans_split       = [float(n) for n in ans.split(' ' )]                     #[lower, upper]
-    stock_increment = 5 #int(input('How many increments for stock price? '))
+    ans             = '150 312'                             #input('Lower and upper stock prices you are examining? ')
+    ans_split       = [float(n) for n in ans.split(' ' )]   #[lower, upper]
+    stock_increment = 13 #int(input('How many increments for stock price? '))
     
-    stock_array    = np.linspace(ans_split[0], ans_split[1], stock_increment)
-    stock_array = stock_array.round(decimals=2) #rounds the array to 2 decimals. 
+    stock_array     = np.linspace(ans_split[0], ans_split[1], stock_increment)
+    stock_array     = stock_array.round(decimals=2)             #rounds the array to 2 decimals. 
     
-    ans_1           = '10 20' #input('Lower and upper volatilities you are examining (%)? ')
-    ans_split1      = [float(n) for n in ans_1.split(' ' )]                     #[lower, upper]
-    vol_increment   = 5 #int(input('How many increments for vol? '))
+    ans_1           = '8 20' #input('Lower and upper volatilities you are examining (%)? ')
+    ans_split1      = [float(n) for n in ans_1.split(' ' )] #[lower, upper]
+    vol_increment   = 13 #int(input('How many increments for vol? '))
         
     vol_array       = np.linspace(ans_split1[0], ans_split1[1], vol_increment)
-    vol_array = vol_array.round(decimals=2) #rounds the array to 2 decimals. 
+    vol_array       = vol_array.round(decimals=2)                 #rounds the array to 2 decimals. 
     
     matrix = matrix_maker(len(stock_array),len(vol_array))
     
-    final = put_shit_in_matrix(stock_array, vol_array, matrix)
-    print(final)
+    strikeprice     = float(input('What is the strike price? '))
+    TimeToMat       = float(input('How long until expiration? (days) '))
+    rfr             = float(input('What is the risk free rate? (as decimal) '))
+    divY            = float(input('What is the divident yield? (as decimal) '))
+    
+    complete_matrix = put_shit_in_matrix(stock_array, vol_array, matrix, 
+                                         strikeprice, TimeToMat, rfr, divY)
+    # S, v, K, T, r, D
+    
+    print(complete_matrix)
     print(vol_array)
     print(stock_array)
     
-    return plotter(final, stock_array, vol_array, stock_increment, vol_increment)
+    return plotter(complete_matrix, stock_array, vol_array, stock_increment, vol_increment)
     
 def matrix_maker(yaxis, xaxis):
     """
@@ -52,20 +66,22 @@ def matrix_maker(yaxis, xaxis):
     
     return matrix
 
-def put_shit_in_matrix(stock_list, volatility_list, matrix):
+def put_shit_in_matrix(stock_list, volatility_list, matrix, strike, TimeToMat, rfr, divY):
     """
     uses the values in stock_list and volatility_list to calulate the respective 
     option premium
     """
     
-    for i in range(len(stock_list)):            #array of stock prices
-        for n in range(len(volatility_list)):   #array of volatilities
-            print(volatility_list[n]/100)
-            matrix[i][n] = BlackScholes_1(stock_list[i], (volatility_list[n]/100)) #need to divide by 100 bc given as %
+    for i in range(len(stock_list)):                        #array of stock prices
+        for n in range(len(volatility_list)):               #array of volatilities
+            matrix[i][n] = BlackScholes_1(stock_list[i], (volatility_list[n]/100), 
+                  strike, TimeToMat, rfr, divY) #need to divide by 100 bc given as %
+            # S, v, K, T, r, D
+            
             #puts the black scholes value in the matrix
     return matrix    
         
-def BlackScholes_1(S, v):
+def BlackScholes_1(S, v, K, T, r, d):
     """
     this function calculates the option 
     premium given a spot and vol value
@@ -81,15 +97,16 @@ def BlackScholes_1(S, v):
     """
     CallPutFlag = 'c'
 #    S = 196.2       #spot price
-    K = 196         #strike prive
-    T = 14/365      #time to maturity
-    r = .02         #risk free rate
-    d = .03         #dividend yield
+#    K = 196         #strike prive
+#    T = 14/365      #time to maturity
+#    r = .02         #risk free rate
+#    d = .03         #dividend yield
 #    v = .13         #percentage as decimal
     
 #    print('new run')
 #    print(S)
 #    print(v)
+    T = T/365                   #Black Scholes formula takes T as % of a year
     
     d1 = (log(float(S)/K)+((r-d)+v*v/2.)*T)/(v*sqrt(T))
     d2 = d1-v*sqrt(T)
@@ -112,10 +129,9 @@ def plotter(matrix, y_label, x_label, yincre, xincre):
     data = matrix
     for y in range(data.shape[0]):
         for x in range(data.shape[1]):
-            plt.text(x + 0.5, y +0.5, '%.4f' % data[y, x], #data[y,x] +0.05 , data[y,x] + 0.05
+            plt.text(x + 0.5, y +0.5, '%.2f' % data[y, x], #data[y,x] +0.05 , data[y,x] + 0.05
                  horizontalalignment='center',
                  verticalalignment='center')
-
 
     ax.autoscale(tight=True)  # get rid of whitespace in margins of heatmap
     ax.set_aspect('equal')  # ensure heatmap cells are square
